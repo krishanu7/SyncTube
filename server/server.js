0d1af794259c98342e5dd5354353d7355ce64e51
Krishanu Saha <krishanu1137@gmail.com>
2024-07-15 12:21:56 +0530

    Frontend URL

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 // Set up CORS for Express
 app.use(cors({
-  origin: "https://watchtogether-live.netlify.app/",
+  origin: "https://watchtogether-live.netlify.app",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
   credentials: true
@@ -20,7 +20,7 @@ const io = new Server(server, {
   path: "/socket",
   serveClient: false,
   cors: {
-    origin: "https://watchtogether-live.netlify.app/",
+    origin: "https://watchtogether-live.netlify.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true
